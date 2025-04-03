56f5d07b4b026f16c3b865fef41d607f5fb490d1
Sascha Makela <sascha.makela@outlook.com>
2025-04-03 20:40:33 +0200

    Add icons

---
+++ CafeMaestro/Resources/Fonts/FontIcons.cs
@@ -0,0 +1,17 @@
+namespace CafeMaestro.Resources.Fonts
+{
+    public static class FontIcons
+    {
+        // Play icon (triangle pointing right)
+        public const string Play = "\u25B6";
+        
+        // Pause icon (two vertical bars)
+        public const string Pause = "\u23F8";
+        
+        // Stop icon (square)
+        public const string Stop = "\u23F9";
+        
+        // Reset icon (circular arrow)
+        public const string Reset = "\u21BA";
+    }
+}
